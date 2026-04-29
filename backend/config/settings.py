005472a303a9f38a74b7311d2bd53360c0c65342
ahmad <ahmad@gmail.com>
2026-04-29 05:45:32 +0000

    added the  some setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,6 +16,7 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
+    'config.apps.ConfigConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
